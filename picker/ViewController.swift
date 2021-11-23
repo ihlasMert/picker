d3b46e62d6b037541d534d561401a1fdd1e5b752
İhlas Mert <ihlas1mert@gmail.com>
2021-11-23 21:33:24 +0300

    Initial Commit

---
+++ picker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  picker
+//
+//  Created by ihlas on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
